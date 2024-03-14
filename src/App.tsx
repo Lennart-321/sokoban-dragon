a869237f2a6044014b2e3035f3d6282d3930b6ad
Lennart-321 <lnnrt0@gmail.com>
2024-03-14 10:36:58 +0100

    t54-level-selection - Menu component sets only the level-index with setLevelIndex from App.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import "./App.css";
 import { GameState } from "./classes/GameState";
 import { GameBoard } from "./components/GameBoard";
 import { Menu } from "./components/Menu";
+import { Levels } from "./classes/Levels";
 
 const dummyGameState = new GameState([[1]]);
 
 function App() {
-    const [level, setLevel] = useState<GameState>(dummyGameState);
+    const [game, setGame] = useState<GameState>(dummyGameState);
+
+    function setLevelIndex(index: number) {
+        setGame(Levels.getGameState(index));
+    }
 
     return (
         <>
-            <Menu setLevel={setLevel} />
-            <GameBoard gameBoard={level} />
+            <Menu setLevel={setLevelIndex} />
+            <GameBoard gameBoard={game} />
         </>
     );
 }
