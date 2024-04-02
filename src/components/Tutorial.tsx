75c7b043037bb34d557c1aba5aaef664aa5cf638
Göran Olson <goran@famolson.se>
2024-04-02 14:04:54 +0200

    t38-redesign-help - updated information modal

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,12 +1,14 @@
 import { Dispatch, SetStateAction } from "react";
 import "../css/tutorial.css";
 import StartScreen from "./StartScreen";
+
 export interface ITutorial {
     showTutorial: boolean;
     setShowTutorial: Dispatch<SetStateAction<boolean>>;
 }
 
 export function Tutorial({ showTutorial, setShowTutorial }: ITutorial) {
+    // Hinder modal closing if click on the modal
     const stopClosing = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         event.stopPropagation();
     }
@@ -14,13 +16,14 @@ export function Tutorial({ showTutorial, setShowTutorial }: ITutorial) {
     return showTutorial ? (
         <>
             <div id="tutorial-modal-outer" onClick={() => setShowTutorial(false)}>
-                <span className="close">&times;</span>
-                <div id="tutorial-modal-inner" onClick={ (e) => stopClosing(e) }>
-                    <span className="close" onClick={() => setShowTutorial(false)}>&times;</span>
-                    <h3 className="header">Instruktioner</h3>
+                <div id="tutorial-modal-inner" onClick={(e) => stopClosing(e)}>
+                    <h3 className="header">
+                        Instruktioner
+                        <span className="close" onClick={() => setShowTutorial(false)}>&times;</span>
+                    </h3>
                     <StartScreen />
                     <div className="creater-container">
-                        <h3>Spelet skapades av:</h3>
+                        <h3 className="header">Spelet skapades 2024 vid ett grupparbete i React av:</h3>
                         <div className="creaters">
                             <div className="name">Anders Stenhammar</div>
                             <div className="name">Göran Olson</div>
@@ -29,7 +32,6 @@ export function Tutorial({ showTutorial, setShowTutorial }: ITutorial) {
                         </div>
                     </div>
                 </div>
-                
             </div>
         </>
     ) : (
